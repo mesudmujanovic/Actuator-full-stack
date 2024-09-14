df95f2d8fe76420d1f0354e30db17ede58b600fb
Mesud Mujanovic <93776713+mesudmujanovic@users.noreply.github.com>
2024-09-14 22:39:12 +0200

    Delete Backend/src/main/java/com/actuator directory

--- Backend/src/main/java/com/actuator/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.actuator.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
